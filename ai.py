32008f7484781c704631f1d41a85dfd46a6b22e4
reza fathi <r.fathi@kaman.ir>
2023-01-06 01:53:10 +0330

    feat(): use algorithm in ai

--- ai.py
+++ ai.py
@@ -1,24 +1,19 @@
 import json
-
-
-# *** you can change everything except the name of the class, the act function and the problem_data ***
+from algorithm import BacktrackingSudoku
 
 
 class AI:
-    # ^^^ DO NOT change the name of the class ***
-
     def __init__(self):
         pass
 
     # the solve function takes a json string as input
     # and outputs the solved version as json
     def solve(self, problem):
-        # ^^^ DO NOT change the solve function above ***
-
         problem_data = json.loads(problem)
-        # ^^^ DO NOT change the problem_data above ***
 
-        # TODO implement your code here
+        # backtracking as local-search
+        backtracking = BacktrackingSudoku(problem_data.get("sudoku"))
+        backtracking.search()
+        solution = backtracking.get_result()
 
-        # finished is the solved version
-        return finished
+        return solution
